dbf4878b10a3e2d63b89da674dfd30728beef5e1
Dikshit <dikshit19csu379@ncuindia.edu>
2022-08-13 03:40:23 +0530

    pending 5d.3

---
+++ ArshSheet/strings/wordWrap36.java
@@ -0,0 +1,16 @@
+package ArshSheet.strings;
+
+public class wordWrap36 {
+    public static int solveWordWrap(int[] nums, int k) {
+        int i = 0;
+        int cost_curr = 0;
+        int cost_min = Integer.MAX_VALUE;
+        return i;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 3, 2, 2, 5 };
+        int k = 6;
+        System.out.println(solveWordWrap(nums, k));
+    }
+}
